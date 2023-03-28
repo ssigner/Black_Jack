6bcfd8bc1238c8d53de4d4210111c98b1c776664
huni00807 <huni00807@gmail.com>
2023-03-28 11:18:47 +0900

    딜러포인트 안보이는 문제 수정

--- js/term.js
+++ js/term.js
@@ -115,6 +115,8 @@ function playStart(){
   if(dealerPoint == 21){
     if(dealerCard[0] == 1) document.getElementById("dealer_point").innerHTML = 1;
     else document.getElementById("dealer_point").innerHTML = 10;
+  } else {
+    document.getElementById("dealer_point").innerHTML = dealerPoint - dealerCard[1];
   }
   if(playerPoint == 21){
     document.getElementById("decision").innerHTML = 
